242eaa258c43de5fa63e3083f82b39fd22805c72
Teekay <tkmaile96@gmail.com>
2024-08-01 11:46:58 +0200

    created my second function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let isAlive = true;
 let message = ""; //an empty string
 
 let messageEl = document.getElementById("message-el");
-let sumeEl = document.getElementById("sum-el");
+let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 let playerEl = document.getElementById("player-el");
 
@@ -15,6 +15,29 @@ playerEl.textContent = player.name + ": $" + player.chips;
 
 // first functoion
  function getRandomcard() {
-    let randomNumber = math.floor(math.random() * 13) + 1;
-    console.log(randomNumber)
+    let randomNumber = math.floor(math.random() * 13) + 1; //Cards from 1 to 13
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
  }
+  // Second function
+
+  function startGame() {
+    isAlive = true;
+    hasBlackJack = false;
+    cards =  [];
+    sum = 0;
+
+    // adding cards to my array
+    let firstCard = getRandomcard();
+    let secondCard = getRandomcard();
+    cards.push(firstCard + secondCard);
+    sum = firstCard + secondCard;
+
+    renderGame();
+    )
+  }
