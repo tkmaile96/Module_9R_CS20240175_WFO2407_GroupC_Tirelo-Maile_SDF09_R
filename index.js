6e52379c243af647467670b3410903399d6aff61
Teekay <tkmaile96@gmail.com>
2024-08-01 11:28:16 +0200

    created my variables

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
-let firstCard = 6
-let secondCard = 9
-
-let sum = firstCard + secondCard
-
-console.log(sum)
+let cards = []; //an empty array
+let sum = 0;
