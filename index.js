94618e5098aa0837759437f615ccee420da8202d
Teekay <tkmaile96@gmail.com>
2024-08-01 11:57:01 +0200

    created my third function

--- index.js
+++ index.js
@@ -39,5 +39,28 @@ playerEl.textContent = player.name + ": $" + player.chips;
     sum = firstCard + secondCard;
 
     renderGame();
-    )
+    
+  }
+
+  // Third Function
+
+  function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for ( let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!"
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
   }
